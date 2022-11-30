37205b9aa343687710261afbaa065541d634499b
chimalexoficial <you@example.com>
2022-11-30 15:54:25 -0800

    budget validation

---
+++ src/components/ControlPresupuesto.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const ControlPresupuesto = () => {
+    return (
+        <div className='contenedor-presupuesto contenedor sombra dos-columnas'>
+
+            <div>
+                <p>Grafica aqui</p>
+            </div>
+
+            <div className='contenido-presupuesto'>
+                <p>
+                    <span>Presupuesto: </span> $200
+                </p>
+            </div>
+
+        </div>
+    )
+}
+
+export default ControlPresupuesto;
